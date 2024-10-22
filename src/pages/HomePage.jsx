319e287eb416794666d2ccf862314d3a8b9f46ae
abdullahhussaintx <abdullah.hussain@txdynamics.io>
2024-10-22 16:23:25 +0500

    details added to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Loader } from "../components/Loader";
 import { useEffect } from "react";
 import { fetchActivationCode } from "../redux/activationCodeSlice";
+import { details, notes } from "../utils/data";
 
 export const HomePage = () => {
   const { loading, error, activationCode } = useSelector(
@@ -21,11 +22,29 @@ export const HomePage = () => {
   }
 
   return (
-    <div className=" flex items-center flex-col gap-5 justify-center ">
-      <p className=" text-2xl text-center">
+    <div className=" flex  flex-col gap-2  ">
+      <p className=" text-2xl underline ">
         Activation Code Fetched Sucessfully
       </p>
       <p className=" text-xl">Please navigate to Tasks</p>
+      <p className=" font-semibold text-lg">Project Details : </p>
+
+      <p className=" font-semibold text-lg">Technologies used : </p>
+      <ul>
+        {details.map((item) => (
+          <li className=" list-inside list-disc" key={item.id}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
+      <p className=" font-semibold text-lg">Notes</p>
+      <ul>
+        {notes.map((item) => (
+          <li className=" list-inside list-disc" key={item.id}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/utils/data.js
+++ src/utils/data.js
@@ -25,3 +25,60 @@ export const menuItmes = [
 export const mediaData = {
   logo,
 };
+
+export const details = [
+  {
+    id: 1,
+    title: "React js",
+  },
+  {
+    id: 2,
+    title: "Tailwind CSS",
+  },
+  {
+    id: 3,
+    title: "Redux tool kit for state management",
+  },
+  {
+    id: 4,
+    title: "React Router Dom for Naviagation",
+  },
+  {
+    id: 5,
+    title: "Axios for api calling",
+  },
+  {
+    id: 6,
+    title: "React icons",
+  },
+  {
+    id: 7,
+    title: "React Tostify for notification",
+  },
+  {
+    id: 8,
+    title: "Reacct Data table Component for displaying data in table",
+  },
+  {
+    id: 9,
+    title: "Leaflet for maps",
+  },
+];
+
+export const notes = [
+  {
+    id: 1,
+    title:
+      "For Task 1 In the PDF tasks provided the Screenshot have a table having  multiple keys and values like reviews roles profile pic and other but in the response of API i only get some of the fields so i have mapped the data accoring to the API response ",
+  },
+  {
+    id: 2,
+    title:
+      "For Task 2 the reponse have some incorrect key values for longitude and latitude so all the employes having correct positions have been displayed on map other i have put them in console.warn",
+  },
+  {
+    id: 3,
+    title:
+      "For Task 3 I have created a Form according to the provided parameters in the PDF not according to the screenshot ",
+  },
+];
